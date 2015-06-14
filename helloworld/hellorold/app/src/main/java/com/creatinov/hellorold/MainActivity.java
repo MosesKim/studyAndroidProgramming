e0f7fe36f15bc2e7581ac6eb8c96f1c38ce59ffd
Moses Kim <kim.moses@cashslide.co.kr>
2015-06-14 19:35:44 +0900

    initial commit helloWorld example #1

---
+++ helloworld/hellorold/app/src/main/java/com/creatinov/hellorold/MainActivity.java
@@ -0,0 +1,56 @@
+package com.creatinov.hellorold;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButton1Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.creatinov.org"));
+        startActivity(myIntent);
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-6641-1040"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent myIntent = new Intent(getApplicationContext(), NewActivity.class);
+        startActivity(myIntent);
+    }
+}
